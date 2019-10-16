f80e63eb6bbe91da7279af91f7332f3597d08536
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 09:10:32 +0200

    aa

--- processi_app/Program.cs
+++ processi_app/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
+
 
 namespace processi_app
 {
@@ -10,6 +12,11 @@ namespace processi_app
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Esercitazione sulla gestione");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe",@"file\Helloworld.txt");
+
+            Process.Start("Chrome.exe", @"https://www.youtube.com/watch?v=-EN7-zPT4cE");
         }
     }
 }
